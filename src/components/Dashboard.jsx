80fbebd954e0b7e7969ba1a61d2a04df255fea4a
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2019-11-16 11:32:26 +0530

    Dashboard design

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,247 @@
 import React from "react"
+import { withRouter } from "react-router-dom"
+import withStyles from "@material-ui/core/styles/withStyles"
+import {
+  Paper,
+  Grid,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel
+} from "@material-ui/core"
 
-const Dashboard = props => <div>Hey this is Dashboard page</div>
-export default Dashboard
+const columns = [
+  { id: "name", label: "ULB", minWidth: 170 },
+  { id: "code", label: "Parcels", minWidth: 100 },
+  {
+    id: "population",
+    label: "Buildings",
+    minWidth: 170,
+    align: "left",
+    format: value => value.toLocaleString()
+  },
+  {
+    id: "size",
+    label: "Portions",
+    minWidth: 170,
+    align: "left",
+    format: value => value.toLocaleString()
+  },
+  {
+    id: "density",
+    label: "Agents",
+    minWidth: 170,
+    align: "left",
+    format: value => value.toFixed(2)
+  }
+]
+
+function createData(name, code, population, size) {
+  const density = population / size
+  return { name, code, population, size, density }
+}
+
+const rows = [
+  createData("India", "IN", 1324171354, 3287263),
+  createData("China", "CN", 1403500365, 9596961),
+  createData("Italy", "IT", 60483973, 301340),
+  createData("United States", "US", 327167434, 9833520),
+  createData("Canada", "CA", 37602103, 9984670),
+  createData("Australia", "AU", 25475400, 7692024),
+  createData("Germany", "DE", 83019200, 357578),
+  createData("Ireland", "IE", 4857000, 70273),
+  createData("Mexico", "MX", 126577691, 1972550),
+  createData("Japan", "JP", 126317000, 377973),
+  createData("France", "FR", 67022000, 640679),
+  createData("United Kingdom", "GB", 67545757, 242495),
+  createData("Russia", "RU", 146793744, 17098246),
+  createData("Nigeria", "NG", 200962417, 923768),
+  createData("Brazil", "BR", 210147125, 8515767)
+]
+
+const styles = theme => ({
+  roots: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 300
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  },
+  root: {
+    width: "100%",
+    margin: "5px 20px 5px 20px"
+  },
+  tableWrapper: {
+    maxHeight: 440,
+    overflow: "auto"
+  },
+  label: {
+    fontSize: "20px"
+  }
+})
+
+const Dashboard = props => {
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
+  const { classes } = props
+  return (
+    <div className={classes.roots}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper
+            className={classes.paper}
+            style={{ backgroundColor: "#d16d36", color: "white" }}
+          >
+            ULB REPORT
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm md={4}>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              All Agencies
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm md={4}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              All Districts
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm md={4}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Lifetime
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={11}>
+          <Paper className={classes.root}>
+            <div className={classes.tableWrapper}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map(column => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                      >
+                        <b className={classes.label}> {column.label}</b>
+                        <TableSortLabel active={true}></TableSortLabel>
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map(row => {
+                      return (
+                        <TableRow
+                          hover
+                          role="checkbox"
+                          tabIndex={-1}
+                          key={row.code}
+                        >
+                          {columns.map(column => {
+                            const value = row[column.id]
+                            return (
+                              <TableCell key={column.id} align={column.align}>
+                                {column.format && typeof value === "number"
+                                  ? column.format(value)
+                                  : value}
+                              </TableCell>
+                            )
+                          })}
+                        </TableRow>
+                      )
+                    })}
+                </TableBody>
+              </Table>
+            </div>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              backIconButtonProps={{
+                "aria-label": "previous page"
+              }}
+              nextIconButtonProps={{
+                "aria-label": "next page"
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default withStyles(styles)(withRouter(Dashboard))
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,9 +6,10 @@ import {
   Typography,
   Container
 } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
+import withStyles from "@material-ui/core/styles/withStyles"
+import { Link } from "react-router-dom"
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -16,58 +17,70 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center"
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%",
     marginTop: theme.spacing(1)
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
   }
-}))
+})
 
-const Login = () => {
-  const classes = useStyles()
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Log in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Log In
-          </Button>
-        </form>
-      </div>
-    </Container>
-  )
+const goToDashboard = e => {
+  e.preventDefault()
+  this.props.history.push("/dashboard")
 }
 
-export default Login
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { classes } = this.props
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Link to="/dashboard">
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Log In
+              </Button>
+            </Link>
+          </form>
+        </div>
+      </Container>
+    )
+  }
+}
+
+export default withStyles(styles)(Login)
